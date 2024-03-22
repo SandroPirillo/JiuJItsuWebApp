8e768c0b3f0783e3722b8f21cb9a5c9de2736933
SandroPirillo <sandropirillo@hotmail.com>
2024-03-23 10:38:06 +1100

    email functionality for trial class form

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -4,16 +4,17 @@ using System.Net.Mail;
 
 namespace JiuJitsuWebApp.Controllers
 {
-    public class ContactController : BaseController
+	public class ContactController : BaseController
 	{
-        public ActionResult ContactUs()
-        {
+		public ActionResult ContactUs()
+		{
 			ViewBag.ThankYouMessage = TempData["ThankYouMessage"] as string;
 			return View();
-        }
+		}
 
 		public ActionResult TrialClass()
 		{
+			ViewBag.ThankYouMessage = TempData["ThankYouMessage"] as string;
 			return View();
 		}
 
@@ -26,10 +27,10 @@ namespace JiuJitsuWebApp.Controllers
 				EnableSsl = true
 			};
 			//validation of the form
-			if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname) || string.IsNullOrEmpty(email) 
+			if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname) || string.IsNullOrEmpty(email)
 				|| string.IsNullOrEmpty(phone) || string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(message))
 			{
-				RedirectToAction("ContactUs");
+				return View("Error");
 			}
 
 			string Inquiry = "First Name: " + firstname + "\n" +
@@ -43,6 +44,48 @@ namespace JiuJitsuWebApp.Controllers
 			return RedirectToAction("ContactUs");
 		}
 
+		[HttpPost]
+		public ActionResult TrialClass(string firstname, string lastname, string parentname, string email, string phone, string classtype, string source, string message)
+		{
+			var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+			{
+				Credentials = new NetworkCredential("61e896bbc7725f", "99881ecf294538"),
+				EnableSsl = true
+			};
+
+			//validation of the form
+			if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname) || string.IsNullOrEmpty(email)
+				|| string.IsNullOrEmpty(phone) || string.IsNullOrEmpty(classtype) || (classtype.Contains("Kids") && string.IsNullOrEmpty(parentname)))
+			{
+				return View("Error");
+			}
+			string Inquiry = "First Name: " + firstname + "\n" +
+							 "Last Name: " + lastname + "\n" +
+							 "Email: " + email + "\n" +
+							 "Phone: " + phone + "\n" +
+							 "Class Type: " + classtype;
+
+			if (!string.IsNullOrEmpty(parentname))
+			{
+				Inquiry += "\nParent Name: " + parentname;
+			}
+
+			if (!string.IsNullOrEmpty(source))
+			{
+				Inquiry += "\nHow did you hear about us: " + source;
+			}
+
+			if (!string.IsNullOrEmpty(message))
+			{
+				Inquiry += "\nMessage: " + message;
+			}
+
+			client.Send(email, "sandropirillo@hotmail.com", "Trial class booking", Inquiry);
+			TempData["ThankYouMessage"] = "Thank you for choosing to start your jiu jitsu journey, we look forward to meeting you";
+			return RedirectToAction("TrialClass");
+		}
+
 	}
 
 }
+
